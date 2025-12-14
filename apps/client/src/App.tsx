c136c7fae349e43b4cf05fb6920964a5bc41f4d8
amrz17 <muhammad.amer@gmail.com>
2025-12-14 21:12:38 +0700

    template react & nest js monorepo

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [greeting, setGreeting] = useState('')
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.text())
+      .then(setGreeting);
+  }, []);
+
+  return (
+    <>
+      <div>
+        {greeting}        
+      </div>
+    </>
+  )
+}
+
+export default App
