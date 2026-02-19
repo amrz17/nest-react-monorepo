161633d7e71cacf88392fbe9cf62587f0116dfa3
amrz17 <muhammad.amer45@gmail.com>
2026-02-19 15:33:31 +0700

    feat(chart dashboard) add dummy data for tren inbound and outbound

--- apps/client/src/components/chart-area-interactive.tsx
+++ apps/client/src/components/chart-area-interactive.tsx
@@ -33,110 +33,110 @@ import {
 export const description = "An interactive area chart"
 
 const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
+  { date: "2024-04-01", inbound: 222, outbound: 150 },
+  { date: "2024-04-02", inbound: 97, outbound: 180 },
+  { date: "2024-04-03", inbound: 167, outbound: 120 },
+  { date: "2024-04-04", inbound: 242, outbound: 260 },
+  { date: "2024-04-05", inbound: 373, outbound: 290 },
+  { date: "2024-04-06", inbound: 301, outbound: 340 },
+  { date: "2024-04-07", inbound: 245, outbound: 180 },
+  { date: "2024-04-08", inbound: 409, outbound: 320 },
+  { date: "2024-04-09", inbound: 59, outbound: 110 },
+  { date: "2024-04-10", inbound: 261, outbound: 190 },
+  { date: "2024-04-11", inbound: 327, outbound: 350 },
+  { date: "2024-04-12", inbound: 292, outbound: 210 },
+  { date: "2024-04-13", inbound: 342, outbound: 380 },
+  { date: "2024-04-14", inbound: 137, outbound: 220 },
+  { date: "2024-04-15", inbound: 120, outbound: 170 },
+  { date: "2024-04-16", inbound: 138, outbound: 190 },
+  { date: "2024-04-17", inbound: 446, outbound: 360 },
+  { date: "2024-04-18", inbound: 364, outbound: 410 },
+  { date: "2024-04-19", inbound: 243, outbound: 180 },
+  { date: "2024-04-20", inbound: 89, outbound: 150 },
+  { date: "2024-04-21", inbound: 137, outbound: 200 },
+  { date: "2024-04-22", inbound: 224, outbound: 170 },
+  { date: "2024-04-23", inbound: 138, outbound: 230 },
+  { date: "2024-04-24", inbound: 387, outbound: 290 },
+  { date: "2024-04-25", inbound: 215, outbound: 250 },
+  { date: "2024-04-26", inbound: 75, outbound: 130 },
+  { date: "2024-04-27", inbound: 383, outbound: 420 },
+  { date: "2024-04-28", inbound: 122, outbound: 180 },
+  { date: "2024-04-29", inbound: 315, outbound: 240 },
+  { date: "2024-04-30", inbound: 454, outbound: 380 },
+  { date: "2024-05-01", inbound: 165, outbound: 220 },
+  { date: "2024-05-02", inbound: 293, outbound: 310 },
+  { date: "2024-05-03", inbound: 247, outbound: 190 },
+  { date: "2024-05-04", inbound: 385, outbound: 420 },
+  { date: "2024-05-05", inbound: 481, outbound: 390 },
+  { date: "2024-05-06", inbound: 498, outbound: 520 },
+  { date: "2024-05-07", inbound: 388, outbound: 300 },
+  { date: "2024-05-08", inbound: 149, outbound: 210 },
+  { date: "2024-05-09", inbound: 227, outbound: 180 },
+  { date: "2024-05-10", inbound: 293, outbound: 330 },
+  { date: "2024-05-11", inbound: 335, outbound: 270 },
+  { date: "2024-05-12", inbound: 197, outbound: 240 },
+  { date: "2024-05-13", inbound: 197, outbound: 160 },
+  { date: "2024-05-14", inbound: 448, outbound: 490 },
+  { date: "2024-05-15", inbound: 473, outbound: 380 },
+  { date: "2024-05-16", inbound: 338, outbound: 400 },
+  { date: "2024-05-17", inbound: 499, outbound: 420 },
+  { date: "2024-05-18", inbound: 315, outbound: 350 },
+  { date: "2024-05-19", inbound: 235, outbound: 180 },
+  { date: "2024-05-20", inbound: 177, outbound: 230 },
+  { date: "2024-05-21", inbound: 82, outbound: 140 },
+  { date: "2024-05-22", inbound: 81, outbound: 120 },
+  { date: "2024-05-23", inbound: 252, outbound: 290 },
+  { date: "2024-05-24", inbound: 294, outbound: 220 },
+  { date: "2024-05-25", inbound: 201, outbound: 250 },
+  { date: "2024-05-26", inbound: 213, outbound: 170 },
+  { date: "2024-05-27", inbound: 420, outbound: 460 },
+  { date: "2024-05-28", inbound: 233, outbound: 190 },
+  { date: "2024-05-29", inbound: 78, outbound: 130 },
+  { date: "2024-05-30", inbound: 340, outbound: 280 },
+  { date: "2024-05-31", inbound: 178, outbound: 230 },
+  { date: "2024-06-01", inbound: 178, outbound: 200 },
+  { date: "2024-06-02", inbound: 470, outbound: 410 },
+  { date: "2024-06-03", inbound: 103, outbound: 160 },
+  { date: "2024-06-04", inbound: 439, outbound: 380 },
+  { date: "2024-06-05", inbound: 88, outbound: 140 },
+  { date: "2024-06-06", inbound: 294, outbound: 250 },
+  { date: "2024-06-07", inbound: 323, outbound: 370 },
+  { date: "2024-06-08", inbound: 385, outbound: 320 },
+  { date: "2024-06-09", inbound: 438, outbound: 480 },
+  { date: "2024-06-10", inbound: 155, outbound: 200 },
+  { date: "2024-06-11", inbound: 92, outbound: 150 },
+  { date: "2024-06-12", inbound: 492, outbound: 420 },
+  { date: "2024-06-13", inbound: 81, outbound: 130 },
+  { date: "2024-06-14", inbound: 426, outbound: 380 },
+  { date: "2024-06-15", inbound: 307, outbound: 350 },
+  { date: "2024-06-16", inbound: 371, outbound: 310 },
+  { date: "2024-06-17", inbound: 475, outbound: 520 },
+  { date: "2024-06-18", inbound: 107, outbound: 170 },
+  { date: "2024-06-19", inbound: 341, outbound: 290 },
+  { date: "2024-06-20", inbound: 408, outbound: 450 },
+  { date: "2024-06-21", inbound: 169, outbound: 210 },
+  { date: "2024-06-22", inbound: 317, outbound: 270 },
+  { date: "2024-06-23", inbound: 480, outbound: 530 },
+  { date: "2024-06-24", inbound: 132, outbound: 180 },
+  { date: "2024-06-25", inbound: 141, outbound: 190 },
+  { date: "2024-06-26", inbound: 434, outbound: 380 },
+  { date: "2024-06-27", inbound: 448, outbound: 490 },
+  { date: "2024-06-28", inbound: 149, outbound: 200 },
+  { date: "2024-06-29", inbound: 103, outbound: 160 },
+  { date: "2024-06-30", inbound: 446, outbound: 400 },
 ]
 
 const chartConfig = {
   visitors: {
     label: "Visitors",
   },
-  desktop: {
-    label: "Desktop",
+  inbound: {
+    label: "Inbound",
     color: "var(--primary)",
   },
-  mobile: {
-    label: "Mobile",
-    color: "var(--primary)",
+  outbound: {
+    label: "Outbound",
+    color: "var(--secondary)",
   },
 } satisfies ChartConfig
 
@@ -165,15 +165,15 @@ export function ChartAreaInteractive() {
   })
 
   return (
-    <Card className="@container/card">
+    <Card className="@container/card px-4">
       <CardHeader>
-        <CardTitle>Total Visitors</CardTitle>
-        <CardDescription>
+        <CardTitle>Tren Inbound vs Outbound</CardTitle>
+        {/* <CardDescription>
           <span className="hidden @[540px]/card:block">
             Total for the last 3 months
           </span>
           <span className="@[540px]/card:hidden">Last 3 months</span>
-        </CardDescription>
+        </CardDescription> */}
         <CardAction>
           <ToggleGroup
             type="single"
@@ -215,27 +215,27 @@ export function ChartAreaInteractive() {
         >
           <AreaChart data={filteredData}>
             <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="fillInbound" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="var(--color-inbound)"
                   stopOpacity={1.0}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="var(--color-inbound)"
                   stopOpacity={0.1}
                 />
               </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="fillOutbound" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="var(--color-outbound)"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="var(--color-outbound)"
                   stopOpacity={0.1}
                 />
               </linearGradient>
@@ -270,17 +270,17 @@ export function ChartAreaInteractive() {
               }
             />
             <Area
-              dataKey="mobile"
+              dataKey="inbound"
               type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-mobile)"
+              fill="url(#fillInbound)"
+              stroke="var(--color-inbound)"
               stackId="a"
             />
             <Area
-              dataKey="desktop"
+              dataKey="outbound"
               type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
+              fill="url(#fillOutbound)"
+              stroke="var(--color-outbound)"
               stackId="a"
             />
           </AreaChart>
